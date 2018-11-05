691d985389f6c761a9ebe3fe7c3a23b7f0ba6e27
karine.mellata@mail.mcgill.ca <karine.mellata@mail.mcgill.ca>
2018-11-05 00:18:03 -0500

    changes

--- a2_lib.c
+++ a2_lib.c
@@ -1,277 +1,247 @@
+//
+// Created by Karine Mellata on 2018-11-01.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "a2_lib.h"
+#include "comp310_a2_test.h"
 
-store_t* gp_Store = NULL;
-int g_fd = -1;
-int cache_valid = MY_FALSE;
-size_t cache_idx = 0;
-char* cache_data[BUCKET_SIZE + 1] = {NULL};
-char* cache_key = NULL;
+//Hashing function taken from tutorial
+int hash(unsigned char *str) {
+    unsigned long hash = 0;
+    int c;
 
-int sm_create(int* fd, char* name, int flags, mode_t perms, int clear) {
-    if(fd == NULL || name == NULL)
-        return MY_ERR_ARG;
+    while (c == *str++)
+        hash = c + (hash << 6) + (hash << 16) - hash;
 
-    *fd = shm_open(name, flags, perms);
-    if(*fd == -1)
-        return MY_ERR;
+    hash = (hash > 0) ? hash % NUM_OF_PODS : -(hash) % NUM_OF_PODS;
 
-    if(clear == MY_TRUE) {
-        int r = ftruncate(*fd, sizeof(store_t));
-        if(r == -1)
-            return MY_ERR;
-    }
-
-    return MY_OK;
+    return (int) hash;
 }
 
-int sm_exists(char* name) {
-    int fd;
-    int r = sm_create(&fd, name, O_RDWR, S_IRWXU, MY_FALSE);
-    if(r == MY_OK)
-        assert(close(fd) == 0);
-    return r;
-}
 
-int sm_attach(int fd, store_t** shm) {
-    if(shm == NULL)
-        return MY_ERR_ARG;
-
-    *shm = (store_t*)mmap(
-            NULL, sizeof(store_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if(*shm == MAP_FAILED) {
-        *shm = NULL;
-        return MY_ERR;
-    }
-
-    return MY_OK;
-}
-
-int sm_detach(store_t** shm) {
-    if(shm == NULL)
-        return MY_ERR_ARG;
-
-    int r = munmap(*shm, sizeof(store_t));
-    if(r == -1)
-        return MY_ERR;
-
-    *shm = NULL;
-
-    return MY_OK;
-}
-
-int sm_close(char* name) {
-    if(name == NULL)
-        return MY_ERR_ARG;
-
-    assert(close(g_fd) == 0);
-
-    int r = shm_unlink(name);
-    if(r == -1)
-        return MY_ERR;
-
-    return MY_OK;
-}
-
-int kv_store_create(char* name) {
-    if(gp_Store != NULL)
-        return 0;
-
-    int r = sm_exists(name);
-    int flag = O_CREAT | O_RDWR;
-    int clear = MY_TRUE;
-
-    if(r == MY_OK) {
-        flag = O_RDWR;
-        clear = MY_FALSE;
-    }
-
-    r = sm_create(&g_fd, name, flag, S_IRWXU, clear);
-    if(r != MY_OK)
-        return -1;
-
-    r = sm_attach(g_fd, &gp_Store);
-    if(r != MY_OK)
-        return -1;
-
-    if(clear == MY_TRUE) {
-        assert(sem_init(&gp_Store->protect, 1, 1) == 0);
-
-        memset(gp_Store->name, 0, sizeof(gp_Store->name));
-        strncpy(gp_Store->name, name, sizeof(gp_Store->name));
-        gp_Store->name[sizeof(gp_Store->name) - 1] = '\0';
-        gp_Store->clients = 0;
-
-        for(size_t i = 0; i < BUCKET_COUNT; i++) {
-            bucket_t* p = &gp_Store->buckets[i];
-
-            p->cache_valid = MY_FALSE;
-
-            cache_idx = 0;
-            cache_valid = MY_FALSE;
-            cache_key = NULL;
-            memset(cache_data, 0, sizeof(cache_data));
-
-            assert(sem_init(&p->protect, 1, 1) == 0);
-            memset(p->keys, 0, sizeof(p->keys));
-            memset(p->values, 0, sizeof(p->values));
+//Initialize bookkeeping information
+int init_info(store* ptr) {
+    memset(ptr, 0, sizeof(store));
+    int idx;
+    //Init pods
+    for(idx = 0; idx < NUM_OF_PODS; idx++){
+        (ptr->pods[idx]).insert_idx = 0;
+        (ptr->pods[idx]).LRU_idx = 0;
+        int i;
+        //Init all LRU objects
+        for(i = 0; i < 10; i++){
+            ((ptr->pods[idx]).distinct_keys[i]).LRU_idx = 0;
         }
     }
+    return 0;
+}
 
-    assert(sem_wait(&gp_Store->protect) == 0);
+//Create a store if not yet created
+//Or open store if already existing
+int kv_store_create(char *name){
+    int fd = shm_open(name, O_CREAT|O_RDWR, 0600); //All permission for owner
+    if(fd < 0){
+        perror("Error ");
+        return -1;
+    }
 
-    // - Sanity check
-    struct stat s;
-    r = fstat(g_fd, &s);
-    if(r == 0)
-        assert(s.st_size == sizeof(store_t));
+    if(ftruncate(fd, sizeof(store)) < 0){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
 
-    gp_Store->clients++;
-    assert(sem_post(&gp_Store->protect) == 0);
+    store *addr = mmap(NULL, sizeof(store) , PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    if(addr == MAP_FAILED){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
+    init_info(addr);
+
+    if(munmap(addr, sizeof(store)) < 0){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
+
+    close(fd);
+    return 0;
+}
+
+int get_key_idx(char* key, int hashed_key, store *sm) {
+    pod key_pod = sm->pods[hashed_key];
+    int idx;
+
+    for(idx = 0; idx < sizeof(key_pod.distinct_keys)/sizeof(key_pod.distinct_keys[0]); idx++) {
+        if(strcmp((((key_pod.distinct_keys[idx]).values[0]).key), key) == 0)
+            return idx;
+    }
+
+    return -1;
+}
+int insert(char* key, char* value, store* ptr, int hashed_key) {
+    kv_entry entry;
+    memset(entry.key, 0, sizeof(entry.key));
+    memset(entry.val, 0, sizeof(entry.val));
+    strncpy(entry.key, key, strlen(key));
+    strncpy(entry.val, value, strlen(value));
+
+
+    int idx = (ptr->pods[hashed_key]).insert_idx;
+    int dist_key_idx = sizeof((ptr->pods[hashed_key]).distinct_keys[idx].values)/sizeof((ptr->pods[hashed_key]).distinct_keys[idx].values[0]) - 1;
+    if(dist_key_idx >= (MAX_VAL - 1)){
+        dist_key_idx = 0;
+    }
+    (ptr->pods[hashed_key]).distinct_keys[idx].values[dist_key_idx] = entry;
+    (ptr->pods[hashed_key]).insert_idx++;
+
+    if((ptr->pods[hashed_key]).insert_idx >= (MAX_VAL - 1)){
+        (ptr->pods[hashed_key]).insert_idx = 0;
+    }
 
     return 0;
 }
 
-void kv_store_print(void) {
-    if(gp_Store == NULL)
-        return;
-
-    assert(sem_wait(&gp_Store->protect) == 0);
-
-    for(size_t i = 0; i < BUCKET_COUNT; i++) {
-        for(size_t j = 0; j < BUCKET_SIZE; j++) {
-            bucket_t* p = &gp_Store->buckets[i];
-            assert(sem_wait(&p->protect) == 0);
-            printf("Bucket %zu | Element %zu | Key='%s' | Value='%s'\n",
-                   i,
-                   j,
-                   p->keys[j],
-                   p->values[j]);
-            assert(sem_post(&p->protect) == 0);
+int modify_entry(char* key, char* value, key_values key_vals) {
+    int i;
+    for(i = 0; i < sizeof((key_vals.values))/sizeof((key_vals.values[0])); i++) {
+        if(key_vals.values[i].key[0] == '\0') {
+            fflush(stdout);
         }
     }
-
-    assert(sem_post(&gp_Store->protect) == 0);
-}
-
-int kv_store_write(char* key, char* value) {
-    if(key == NULL || value == NULL || gp_Store == NULL || strlen(key) < 1)
-        return -1;
-
-    int i = hash_func(key, BUCKET_COUNT);
-    bucket_t* p = &gp_Store->buckets[i];
-
-    assert(sem_wait(&p->protect) == 0);
-
-    p->cache_valid = MY_FALSE;
-    cache_idx = 0;
-    cache_key = NULL;
-    cache_valid = MY_FALSE;
-    for(size_t j = 0; j < BUCKET_SIZE + 1; j++)
-        cache_data[j] = NULL;
-
-    char keys[BUCKET_SIZE][KEY_COUNT];
-    char values[BUCKET_SIZE][KEY_SIZE];
-
-    for(size_t k = 0; k < BUCKET_SIZE; k++) {
-        strncpy(keys[k], p->keys[k], KEY_COUNT);
-        strncpy(values[k], p->values[k], KEY_SIZE);
-    }
-
-    for(size_t k = 0; k < BUCKET_SIZE - 1; k++) {
-        strncpy(p->keys[k + 1], keys[k], KEY_COUNT);
-        strncpy(p->values[k + 1], values[k], KEY_SIZE);
-    }
-
-    strncpy(p->keys[0], key, KEY_COUNT);
-    strncpy(p->values[0], value, KEY_SIZE);
-
-    p->keys[0][KEY_COUNT - 1] = '\0';
-    p->values[0][KEY_SIZE - 1] = '\0';
-
-    assert(sem_post(&p->protect) == 0);
-
     return 0;
 }
+//Take a key-value pair and write to the store
+int kv_store_write(char *key, char *value){
+    int key_len = strlen(key);
+    int val_len = strlen(value);
+    char new_key[MAX_KEY_SIZE + 1];
+    char new_val[MAX_VALUE_SIZE + 1];
+    int last_key_idx = (key_len > __TEST_MAX_KEY_SIZE__) ? MAX_KEY_SIZE : key_len;
+    int last_val_idx = (val_len > __TEST_MAX_DATA_LENGTH__) ? MAX_VALUE_SIZE : val_len;
 
-char* kv_store_read(char* key) {
-    if(key == NULL || gp_Store == NULL)
-        return NULL;
+    strncpy(new_key, key, last_key_idx);
+    new_key[last_key_idx] = '\0';
 
-    int k = hash_func(key, BUCKET_COUNT);
-    char* r = NULL;
-    bucket_t* p = &gp_Store->buckets[k];
+    strncpy(new_val, value, last_val_idx);
+    new_val[last_val_idx] = '\0';
 
-    assert(sem_wait(&p->protect) == 0);
+    int fd = shm_open(__TEST_SHARED_MEM_NAME__, O_CREAT|O_RDWR, 0600); //All permission for owner
+    if(fd < 0){
+        perror("Error ");
+        return -1;
+    }
 
-    if(p->cache_valid == MY_TRUE && cache_valid == MY_TRUE &&
-       strncmp(cache_key, key, KEY_COUNT) == 0) {
-        if(cache_data[cache_idx] == NULL) {
-            r = NULL;
-            cache_idx = 0;
-        } else {
-            r = strndup(cache_data[cache_idx++], KEY_SIZE);
-        }
-    } else {
-        p->cache_valid = MY_TRUE;
-        cache_idx = 0;
-        cache_valid = MY_TRUE;
-        size_t l = 0;
+    store *addr = mmap(NULL, sizeof(store) , PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-        for(size_t i = 0; i < BUCKET_SIZE; i++) {
-            if(strncmp(p->keys[BUCKET_SIZE - i - 1], key, KEY_COUNT) == 0) {
-                cache_key = p->keys[BUCKET_SIZE - i - 1];
-                cache_data[l++] = p->values[BUCKET_SIZE - i - 1];
+    if(addr == MAP_FAILED){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
+
+    if(ftruncate(fd, sizeof(store)) < 0){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
+
+    int hashed_key = hash((unsigned char *)key);
+    int idx = get_key_idx(key, hashed_key, addr);
+
+    insert(new_key, new_val, addr, hashed_key);
+
+    if(munmap(addr, sizeof(store)) < 0){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
+    close(fd);
+    return 0;
+
+}
+
+//Find key and return copy of the value
+char *kv_store_read(char *key){
+    int fd = shm_open(__TEST_SHARED_MEM_NAME__, O_CREAT|O_RDWR, 0600); //All permission for owner
+    if(fd < 0){
+        perror("Error ");
+        return -1;
+    }
+
+    store *addr = mmap(NULL, sizeof(store), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    if(addr == MAP_FAILED){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
+
+    if(ftruncate(fd, sizeof(store)) < 0){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
+
+    char *value = NULL;
+
+    int hashed_key = hash((unsigned char *)key);
+    pod key_pod = (addr->pods[hashed_key]);
+    int i;
+    for(i = 0; i < MAX_VAL; i++){
+        key_values curr_entry = key_pod.distinct_keys[i];
+        char *key = &(curr_entry.values[0].key);
+        if(hashed_key == hash((unsigned char *) key)){
+            value = (char *) calloc(1, sizeof(char) * 32);
+            strcpy(value, curr_entry.values[curr_entry.LRU_idx].val);
+            curr_entry.LRU_idx++;
+            if(curr_entry.LRU_idx >= MAX_VAL - 1){
+                curr_entry.LRU_idx = 0;
             }
+            break;
         }
-
-        cache_data[l] = NULL;
-
-        if(l > 0)
-            r = strndup(cache_data[cache_idx++], KEY_SIZE);
     }
 
-    assert(sem_post(&p->protect) == 0);
+    if(munmap(addr, sizeof(store)) < 0){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
+    close(fd);
+    return value;
 
-    return r;
 }
 
-char** kv_store_read_all(char* key) {
-    if(key == NULL || gp_Store == NULL)
-        return NULL;
-
-    int k = hash_func(key, BUCKET_COUNT);
-    char** values = (char**)calloc(BUCKET_SIZE + 1, sizeof(char*));
-    values[BUCKET_SIZE] = NULL;
-
-    size_t r = 0;
-    bucket_t* p = &gp_Store->buckets[k];
-
-    assert(sem_wait(&p->protect) == 0);
-
-    for(size_t i = 0; i < BUCKET_SIZE; i++) {
-        if(strncmp(p->keys[BUCKET_SIZE - i - 1], key, KEY_COUNT) == 0) {
-            values[r++] = strndup(p->values[BUCKET_SIZE - i - 1], KEY_SIZE);
-        }
+//Take a key and return all values in store
+char **kv_store_read_all(char *key){
+    int fd = shm_open(__TEST_SHARED_MEM_NAME__, O_CREAT|O_RDWR, 0600); //All permission for owner
+    if(fd < 0){
+        perror("Error ");
+        return -1;
     }
 
-    values[r] = NULL;
+    store *addr = mmap(NULL, sizeof(store), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-    if(values[0] == NULL) {
-        assert(sem_post(&p->protect) == 0);
-        free(values);
-        return NULL;
+    if(addr == MAP_FAILED){
+        close(fd);
+        perror("Error ");
+        return -1;
     }
 
-    assert(sem_post(&p->protect) == 0);
-
+    if(ftruncate(fd, sizeof(store)) < 0){
+        close(fd);
+        perror("Error ");
+        return -1;
+    }
+    char **values = (char **) calloc(16, sizeof(char*));
     return values;
 }
 
-size_t hash_func(char* word, int32_t max) {
-    int32_t hashAddress = 5381;
-    for(size_t counter = 0; word[counter] != '\0'; counter++) {
-        hashAddress = hashAddress % (1 << 24);
-        hashAddress = ((hashAddress << 5) + hashAddress) + word[counter];
-    }
-    return ((hashAddress % max) < 0) ? -hashAddress % max : hashAddress % max;
-}
